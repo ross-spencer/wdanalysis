2441b68bd02805afe6d5a37a3074339850ff67ad
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-04-18 21:42:27 -0400

    Enhance analysis capability

--- analysis.go
+++ analysis.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -8,52 +9,50 @@ import (
 	"github.com/ross-spencer/spargo/pkg/spargo"
 )
 
+var (
+	threshold int
+	debug     bool
+	csv       bool
+	trim      int
+)
+
+func init() {
+	flag.IntVar(&threshold, "threshold", 0, "turn threshold on to output (sigs no. > threshold)")
+	flag.BoolVar(&debug, "debug", false, "turn debug debug on to investigate signatures")
+	flag.BoolVar(&csv, "csv", false, "create CSV to investigate signatures")
+	flag.IntVar(&trim, "trim", 0, "trim signatures when outputting csv")
+}
+
+// p:P31 is an instance of a file format.
+
 var url = "https://query.wikidata.org/sparql"
-var query = `SELECT DISTINCT ?format ?formatLabel ?puid ?ldd ?extension ?mimetype ?sig ?referenceLabel ?date ?encodingLabel ?offset ?relativityLabel WHERE
-{
-  ?format wdt:P2748 ?puid.
-  OPTIONAL { ?format wdt:P3266 ?ldd }
-  OPTIONAL { ?format wdt:P1195 ?extension }
-  OPTIONAL { ?format wdt:P1163 ?mimetype }
-  OPTIONAL { ?format wdt:P4152 ?sig }
-  OPTIONAL {
-     ?format p:P4152 ?object.
-     ?object prov:wasDerivedFrom ?provenance.
-     ?provenance pr:P248 ?reference;
-        pr:P813 ?date.
-  }
-  OPTIONAL {
-     ?format p:P4152 ?object.
-     ?object pq:P3294 ?encoding.
-     ?object pq:P4153 ?offset.
-  }
-  OPTIONAL {
-     ?format p:P4152 ?object.
-     ?object pq:P2210 ?relativity.
-  }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}
-order by ?format`
-
-// Wikidata ... might be commented in Siegfried...
-type Wikidata struct {
-	ID        string    // Wikidata short name, e.g. Q12345 can be appended to a URI to be dereferenced.
-	Name      string    // Name of the format as described in Wikidata.
-	URI       string    // URI is the absolute URL in Wikidata terms that can be dereferenced.
-	PRONOM    []string  // 1:1 mapping to PRONOM wherever possible.
-	LOC       []string  // Library of Congress identifiers.
-	Extension []string  // Extension returned by Wikidata.
-	Mimetype  []string  // Mimetype as recorded by Wikidata.
-	Signature Signature // Signature associated with a record which we will convert to a new Type.
-}
-
-// Signature ...
-type Signature struct {
-	Signature  string // Signature byte sequence.
-	Provenance string // Provenance of the signature.
-	Date       string // Date the signature was submitted.
-	Encoding   string // Signature encoding, e.g. Hexadecimal, ASCII, PRONOM.
-}
+var query = `
+		SELECT DISTINCT ?format ?formatLabel ?puid ?ldd ?extension ?mimetype ?sig ?referenceLabel ?date ?encodingLabel ?offset ?relativityLabel WHERE
+		{
+		  ?format wdt:P2748 ?puid.
+		  OPTIONAL { ?format wdt:P3266 ?ldd }
+		  OPTIONAL { ?format wdt:P1195 ?extension }
+		  OPTIONAL { ?format wdt:P1163 ?mimetype }
+		  OPTIONAL { ?format wdt:P4152 ?sig }
+		  OPTIONAL {
+		     ?format p:P4152 ?object.
+		     ?object prov:wasDerivedFrom ?provenance.
+		     ?provenance pr:P248 ?reference;
+		        pr:P813 ?date.
+		  }
+		  OPTIONAL {
+		     ?format p:P4152 ?object.
+		     ?object pq:P3294 ?encoding.
+		     ?object pq:P4153 ?offset.
+		  }
+		  OPTIONAL {
+		     ?format p:P4152 ?object.
+		     ?object pq:P2210 ?relativity.
+		  }
+		  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE], en". }
+		}
+		order by ?format
+`
 
 var wikidataMapping = make(map[string]Wikidata)
 
@@ -66,6 +65,16 @@ const mimeField = "mimetype"
 func getID(wikidataURI string) string {
 	splitURI := strings.Split(wikidataURI, "/")
 	return splitURI[len(splitURI)-1]
+}
+
+func newSignature(wdRecord map[string]spargo.Item) Signature {
+	tmpWD := Signature{}
+	tmpWD.Signature = wdRecord["sig"].Value
+	tmpWD.Provenance = wdRecord["referenceLabel"].Value
+	tmpWD.Date = wdRecord["date"].Value
+	tmpWD.Encoding = wdRecord["encodingLabel"].Value
+	tmpWD.Relativity = wdRecord["relativityLabel"].Value
+	return tmpWD
 }
 
 // Create a newRecord with fields from the query sent to Wikidata.
@@ -104,13 +113,8 @@ func newRecord(wdRecord map[string]spargo.Item) Wikidata {
 	wd.Extension = append(wd.Extension, wdRecord["extension"].Value)
 	wd.Mimetype = append(wd.Mimetype, wdRecord["mimetype"].Value)
 
-	if sig == false {
-		wd.Signature = Signature{}
-	} else {
-		wd.Signature.Signature = wdRecord["sig"].Value
-		wd.Signature.Provenance = wdRecord["referenceLabel"].Value
-		wd.Signature.Date = wdRecord["date"].Value
-		wd.Signature.Encoding = wdRecord["relativityLabel"].Value
+	if sig == true {
+		wd.Signatures = append(wd.Signatures, newSignature(wdRecord))
 	}
 
 	return wd
@@ -125,8 +129,20 @@ func contains(items []string, item string) bool {
 	return false
 }
 
-// A format record has some repeating properties, updateReocrd manages those
-// exceptions.
+func updateSignatures(wd *Wikidata, wdRecord map[string]spargo.Item) {
+	found := false
+	for _, s := range wd.Signatures {
+		if s.Signature == wdRecord["sig"].Value {
+			found = true
+		}
+	}
+	if found == false {
+		wd.Signatures = append(wd.Signatures, newSignature(wdRecord))
+	}
+}
+
+// A format record has some repeating properties. updateRecord manages those
+// exceptions and adds them to the list if it doesn't already exist.
 func updateRecord(wdRecord map[string]spargo.Item, wd Wikidata) Wikidata {
 	if contains(wd.PRONOM, wdRecord[puidField].Value) == false {
 		wd.PRONOM = append(wd.PRONOM, wdRecord[puidField].Value)
@@ -140,17 +156,25 @@ func updateRecord(wdRecord map[string]spargo.Item, wd Wikidata) Wikidata {
 	if contains(wd.Mimetype, wdRecord[mimeField].Value) == false {
 		wd.Mimetype = append(wd.Mimetype, wdRecord[mimeField].Value)
 	}
+	if wdRecord["sig"].Value != "" {
+		updateSignatures(&wd, wdRecord)
+	}
 	return wd
 }
 
-func countSignatures() int {
-	var count int
-	for _, v := range wikidataMapping {
-		if v.Signature.Signature != "" {
-			count++
+func analyseWikidataRecords(summary *Summary) {
+	for _, wd := range wikidataMapping {
+		if len(wd.Signatures) > 1 {
+			summary.MultipleSequences++
+			summary.Multiples = append(summary.Multiples, wd.URI)
+		}
+		for _, signature := range wd.Signatures {
+			signature.analyseSignature(summary, wd.URI)
+		}
+		if len(wd.Signatures) != 0 {
+			summary.FormatsWithSignatures++
 		}
 	}
-	return count
 }
 
 func runSPARQL() []map[string]spargo.Item {
@@ -161,8 +185,9 @@ func runSPARQL() []map[string]spargo.Item {
 }
 
 func main() {
+	flag.Parse()
 	results := runSPARQL()
-	fmt.Fprintf(os.Stderr, "Original SPARQL results: %d\n", len(results))
+	var summary Summary
 	for _, wdRecord := range results {
 		id := getID(wdRecord[formatField].Value)
 		if wikidataMapping[id].ID == "" {
@@ -171,7 +196,29 @@ func main() {
 			wikidataMapping[id] = updateRecord(wdRecord, wikidataMapping[id])
 		}
 	}
-	fmt.Fprintf(os.Stderr, "Condensed SPARQL results: %d\n", len(wikidataMapping))
-	fmt.Fprintf(os.Stderr, "Number of anticipated signatures: %d\n", countSignatures())
-	fmt.Fprintf(os.Stderr, "Report generation complete...\n")
+	summary.AllSparqlResults = len(results)
+	summary.CondensedSparqlResults = len(wikidataMapping)
+	analyseWikidataRecords(&summary)
+	if debug {
+		out := ""
+		for _, wd := range wikidataMapping {
+			if len(wd.Signatures) > threshold {
+				for _, signature := range wd.Signatures {
+					if !csv {
+						out = fmt.Sprintf("%s%s,", out, signature)
+					} else {
+						out = fmt.Sprintf("%s%s\n", out, signature.CSV(wd.URI, len(wd.Signatures)))
+					}
+				}
+			}
+		}
+		if !csv {
+			fmt.Fprintf(os.Stdout, "[%s]", strings.Trim(out, ","))
+			return
+		}
+		const header = "uri, count, sig, provenance, date, encoding, relativity"
+		fmt.Fprintf(os.Stdout, "%s\n%s", header, out)
+	} else {
+		fmt.Fprintf(os.Stdout, "%s\n", summary)
+	}
 }
---
+++ structs.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Wikidata ... might be commented in Siegfried...
+type Wikidata struct {
+	ID         string      // Wikidata short name, e.g. Q12345 can be appended to a URI to be dereferenced.
+	Name       string      // Name of the format as described in Wikidata.
+	URI        string      // URI is the absolute URL in Wikidata terms that can be dereferenced.
+	PRONOM     []string    // 1:1 mapping to PRONOM wherever possible.
+	LOC        []string    // Library of Congress identifiers.
+	Extension  []string    // Extension returned by Wikidata.
+	Mimetype   []string    // Mimetype as recorded by Wikidata.
+	Signatures []Signature // Signature associated with a record which we will convert to a new Type.
+}
+
+// Signature ...
+type Signature struct {
+	Signature  string // Signature byte sequence.
+	Provenance string // Provenance of the signature.
+	Date       string // Date the signature was submitted.
+	Encoding   string // Signature encoding, e.g. Hexadecimal, ASCII, PRONOM.
+	Relativity string // Position relative to beginning or end of file, or elsewhere.
+}
+
+// Serialize the signature component of our record to a string to debug.
+func (s Signature) String() string {
+	report, err := json.MarshalIndent(s, "", "  ")
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%s", report)
+}
+
+// CSV will serialize the signature component of our record to a csv to debug.
+func (s Signature) CSV(uri string, count int) string {
+	provenance := s.Provenance
+	date := s.Date
+	encoding := s.Encoding
+	relativity := s.Relativity
+	signature := s.Signature
+	if provenance == "" {
+		provenance = "None"
+	}
+	if date == "" {
+		date = "None"
+	}
+	if encoding == "" {
+		encoding = "None"
+	}
+	if relativity == "" {
+		relativity = "None"
+	}
+	if len(signature) >= trim && trim > 0 {
+		signature = s.Signature[:trim]
+	}
+	return fmt.Sprintf("%s, %d, %s, %s, %s, %s, %s",
+		uri,
+		count,
+		signature,
+		provenance,
+		date,
+		encoding,
+		relativity,
+	)
+}
+
+func (s Signature) analyseSignature(summary *Summary, uri string) {
+	if s.Provenance == "" {
+		summary.ErrNoProvenance++
+		if uri != "" && !contains(summary.NoProvenance, uri) {
+			summary.NoProvenance = append(summary.NoProvenance, uri)
+		}
+	}
+	if s.Date == "" {
+		summary.ErrNoDate++
+		if uri != "" && !contains(summary.NoDate, uri) {
+			summary.NoDate = append(summary.NoDate, uri)
+		}
+	}
+	if s.Encoding == "" {
+		summary.ErrNoEncoding++
+		if uri != "" && !contains(summary.NoEncoding, uri) {
+			summary.NoEncoding = append(summary.NoEncoding, uri)
+		}
+	}
+	if s.Relativity == "" {
+		summary.ErrNoRelativity++
+		if uri != "" && !contains(summary.NoRelativity, uri) {
+			summary.NoRelativity = append(summary.NoRelativity, uri)
+		}
+	}
+}
+
+// Summary of the identifier.
+type Summary struct {
+	AllSparqlResults       int
+	CondensedSparqlResults int
+	FormatsWithSignatures  int
+	MultipleSequences      int
+	ErrNoProvenance        int
+	ErrNoDate              int
+	ErrNoRelativity        int
+	ErrNoEncoding          int
+
+	// Sets to help understand content.
+	setEncoding []string
+
+	// Records that need investigating.
+	Multiples    []string
+	NoProvenance []string
+	NoDate       []string
+	NoRelativity []string
+	NoEncoding   []string
+}
+
+// String will return a summary report to be printed.
+func (s Summary) String() string {
+	report, err := json.MarshalIndent(s, "", "  ")
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%s", report)
+}
