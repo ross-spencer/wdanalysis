5e82913c4bcf45177bde920cda926a0a9b8becc3
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-03-19 18:04:52 +0100

    WIP: Not sure what these changes do.

--- wdlyzer.go
+++ wdlyzer.go
@@ -1,5 +1,26 @@
 package main
 
+// Maintain a memory of all the linting warnings and errors that are
+// discovered parsing the Wikidata report.
+//
+// Linting is independent of signature processing but provides
+// information about the Wikidata record that is concrete and can be
+// fixed up. Linting is recorded in parallel with processing the
+// signature and returned in one block that can then be accessed.
+//
+// All errors are treated as a signal to stop processing the signature.
+// The signature will not be added to the final identifier and the
+// record will need to be updated in Wikidata itself.
+//
+// Not all linting issues are 'fatal' but some make it difficult to
+// process the signature.
+//
+//
+// Signatures that make it through should all be able to be processed by
+// Siegfried. We try and output some summary information about the
+// signatures.
+//
+
 import (
 	"flag"
 	"fmt"
@@ -23,104 +44,124 @@ func init() {
 	flag.IntVar(&trim, "trim", 0, "trim signatures when outputting csv")
 }
 
-// p:P31 is an instance of a file format.
-
-var url = "https://query.wikidata.org/sparql"
-var query = `
-	SELECT DISTINCT ?format ?formatLabel ?puid ?ldd ?extension ?mimetype ?sig ?referenceLabel ?date ?encodingLabel ?offset ?relativityLabel WHERE
-	{
-	  ?format wdt:P31/wdt:P279* wd:Q235557.
-	  OPTIONAL { ?format wdt:P2748 ?puid. }
-	  OPTIONAL { ?format wdt:P3266 ?ldd }
-	  OPTIONAL { ?format wdt:P1195 ?extension }
-	  OPTIONAL { ?format wdt:P1163 ?mimetype }
-	  OPTIONAL { ?format wdt:P4152 ?sig }
-	  OPTIONAL {
-	     ?format p:P4152 ?object.
-	     ?object prov:wasDerivedFrom ?provenance.
-	     ?provenance pr:P248 ?reference;
-	        pr:P813 ?date.
-	  }
-	  OPTIONAL {
-	     ?format p:P4152 ?object.
-	     ?object pq:P3294 ?encoding.
-	     ?object pq:P4153 ?offset.
-	  }
-	  OPTIONAL {
-	     ?format p:P4152 ?object.
-	     ?object pq:P2210 ?relativity.
-	  }
-	  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE], en". }
-	}
-	order by ?format
-`
-
-var wikidataMapping = make(map[string]Wikidata)
-
-const formatField = "format"
+const uriField = "uri"
+const formatLabelField = "formatLabel"
 const puidField = "puid"
 const locField = "ldd"
 const extField = "extension"
 const mimeField = "mimetype"
+const signatureField = "sig"
+const offsetField = "offset"
+const encodingField = "encodingLabel"
+const relativityField = "relativityLabel"
+const dateField = "date"
+const referenceField = "referenceLabel"
 
+const langTemplate = "<<lang>>"
+const lang = "en"
+
+var url = "https://query.wikidata.org/sparql"
+
+// Developer note: There is only a loose coupling between the SPARQL
+// variables and the variables used in code. As such, updating this
+// query should be done with care.
+var query = `
+    # Return all file format records from Wikidata.
+    #
+    select distinct ?uri ?uriLabel ?puid ?extension ?mimetype ?encodingLabel ?referenceLabel ?date ?relativityLabel ?offset ?sig
+    where
+    {
+      ?uri wdt:P31/wdt:P279* wd:Q235557.            # Return records of type File Format.
+      optional { ?uri wdt:P2748 ?puid.   }          # PUID is used to map to PRONOM signatures proper.
+      optional { ?uri wdt:P1195 ?extension  }
+      optional { ?uri wdt:P1163 ?mimetype   }
+      # We don't need to require that there is a format identification pattern because
+      # we want to be able to provide results for items without them that are mapped to
+      # PRONOM anyway.
+      optional { ?uri p:P4152 ?object;              # Format identification pattern statement.
+        optional { ?object pq:P3294 ?encoding.   }     # We don't always have an encoding.
+        optional { ?object ps:P4152 ?sig.        }     # We always have a signature.
+        optional { ?object pq:P2210 ?relativity. }     # Relativity to beginning or end of file.
+        optional { ?object pq:P4153 ?offset.     }     # Offset relatve to the relativity.
+
+        optional { ?object prov:wasDerivedFrom ?provenance;
+           optional { ?provenance pr:P248 ?reference;
+                                  pr:P813 ?date.
+                    }
+        }
+      }
+      # Wikidata's mechanism to return labels from SPARQL parameters.
+      service wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE], <<lang>>". }
+    }
+    order by ?uri
+`
+
+// WIKIDATA TODO: Write test for this function...
 func getID(wikidataURI string) string {
 	splitURI := strings.Split(wikidataURI, "/")
 	return splitURI[len(splitURI)-1]
 }
 
-func newSignature(wdRecord map[string]spargo.Item) Signature {
-	tmpWD := Signature{}
-	tmpWD.Signature = wdRecord["sig"].Value
-	tmpWD.Provenance = wdRecord["referenceLabel"].Value
-	tmpWD.Date = wdRecord["date"].Value
-	tmpWD.Encoding = wdRecord["encodingLabel"].Value
-	tmpWD.Relativity = wdRecord["relativityLabel"].Value
-	return tmpWD
-}
-
-// Create a newRecord with fields from the query sent to Wikidata.
-//
-//		"format"	<-- Wikidata URI.
-//		"formatLabel"	<-- Format name.
-//		"puid"	<-- PUID returned by Wikidata.
-//		"extension"	<-- Format extension.
-//		"mimetype"	<-- MimeType as recorded by Wikidata.
-//
-//		TODO: Let's begin with a count of Wikidata signatures
-//			  A format might have multiple signatures that can be used to
-//			  match a record. Signatures might have multiple forms, e.g. Hex,
-//			  or PRONOM regular expression.
-//
-//		"sig"	<-- Signature in Wikidata.
-//		"referenceLabel"	<-- Signature provenance.
-//		"date"	<-- Date the signature was submitted.
-//		"encodingLabel"	<-- Encoding used for a Signature.
-//		"offset"	<-- Offset relative to a position in a file.
-//		"relativityLabel" 	<-- Direction from which to measure an offset for a signature.
-//
-func newRecord(wdRecord map[string]spargo.Item) Wikidata {
-	sig := false
-	if wdRecord["sig"].Value != "" {
-		sig = true
-	}
+// newRecord creates a Wikidata record with the values received from
+// Wikidata itself.
+func newRecord(wdRecord map[string]spargo.Item, addSigs bool) Wikidata {
 	wd := Wikidata{}
-
-	wd.ID = getID(wdRecord["format"].Value)
-	wd.Name = wdRecord["formatLabel"].Value
-	wd.URI = wdRecord["format"].Value
-
-	wd.PRONOM = append(wd.PRONOM, wdRecord["puid"].Value)
-	wd.LOC = append(wd.LOC, wdRecord["ldd"].Value)
-	wd.Extension = append(wd.Extension, wdRecord["extension"].Value)
-	wd.Mimetype = append(wd.Mimetype, wdRecord["mimetype"].Value)
-
-	if sig == true {
-		wd.Signatures = append(wd.Signatures, newSignature(wdRecord))
+	wd.ID = getID(wdRecord[uriField].Value)
+	wd.Name = wdRecord[formatLabelField].Value
+	wd.URI = wdRecord[uriField].Value
+	wd.PRONOM = append(wd.PRONOM, wdRecord[puidField].Value)
+	wd.Extension = append(wd.Extension, wdRecord[extField].Value)
+	wd.Mimetype = append(wd.Mimetype, wdRecord[mimeField].Value)
+	if wdRecord[signatureField].Value != "" {
+		if !addSigs {
+			// Pre-processing has returned no particular heuristic will
+			// help us here and so let's make sure we can report on that
+			// at the end, as well as exit early.
+			addLinting(wd.URI, heuWDE01)
+			wd.disableSignatures = true
+			return wd
+		}
+		sig := Signature{}
+		wd.Signatures = append(wd.Signatures, sig)
+		bs := newByteSequence(wdRecord)
+		wd.Signatures[0].ByteSequences = append(wd.Signatures[0].ByteSequences, bs)
 	}
-
 	return wd
 }
 
+// updateRecord manages a format record's repeating properties.
+// exceptions and adds them to the list if it doesn't already exist.
+func updateRecord(wdRecord map[string]spargo.Item, wd Wikidata) Wikidata {
+	if contains(wd.PRONOM, wdRecord[puidField].Value) == false {
+		wd.PRONOM = append(wd.PRONOM, wdRecord[puidField].Value)
+	}
+	if contains(wd.Extension, wdRecord[extField].Value) == false {
+		wd.Extension = append(wd.Extension, wdRecord[extField].Value)
+	}
+	if contains(wd.Mimetype, wdRecord[mimeField].Value) == false {
+		wd.Mimetype = append(wd.Mimetype, wdRecord[mimeField].Value)
+	}
+	if wdRecord[signatureField].Value != "" {
+		if !wd.disableSignatures {
+			lintingErr := updateSequences(wdRecord, &wd)
+			// WIKIDATA FUTURE: If we can re-organize the signatures in
+			// Wikidata so that they are better encapsulated from each
+			// other then we don't need to be as strict about not
+			// processing the value. Right now, there's not enough
+			// consistency in records that mix signatures with multiple
+			// sequences, types, offsets and so forth.
+			if lintingErr != nle {
+				wd.Signatures = nil
+				wd.disableSignatures = true
+				addLinting(wd.URI, lintingErr)
+			}
+		}
+	}
+	return wd
+}
+
+// contains will look for the appearance of a string  item in slice of
+// strings items.
 func contains(items []string, item string) bool {
 	for i := range items {
 		if items[i] == item {
@@ -130,58 +171,32 @@ func contains(items []string, item string) bool {
 	return false
 }
 
-func updateSignatures(wd *Wikidata, wdRecord map[string]spargo.Item) {
-	found := false
-	for _, s := range wd.Signatures {
-		if s.Signature == wdRecord["sig"].Value {
-			found = true
-		}
-	}
-	if found == false {
-		wd.Signatures = append(wd.Signatures, newSignature(wdRecord))
-	}
-}
-
-// A format record has some repeating properties. updateRecord manages those
-// exceptions and adds them to the list if it doesn't already exist.
-func updateRecord(wdRecord map[string]spargo.Item, wd Wikidata) Wikidata {
-	if contains(wd.PRONOM, wdRecord[puidField].Value) == false {
-		wd.PRONOM = append(wd.PRONOM, wdRecord[puidField].Value)
-	}
-	if contains(wd.LOC, wdRecord[locField].Value) == false {
-		wd.LOC = append(wd.LOC, wdRecord[locField].Value)
-	}
-	if contains(wd.Extension, wdRecord[extField].Value) == false {
-		wd.Extension = append(wd.Extension, wdRecord[extField].Value)
-	}
-	if contains(wd.Mimetype, wdRecord[mimeField].Value) == false {
-		wd.Mimetype = append(wd.Mimetype, wdRecord[mimeField].Value)
-	}
-	if wdRecord["sig"].Value != "" {
-		updateSignatures(&wd, wdRecord)
-	}
-	return wd
-}
-
+// analyseWikidataRecords ...
 func analyseWikidataRecords(summary *Summary) {
+	recordsWithLinting, allLinting, badHeuristics := countLintingErrors()
+	summary.RecordsWithLintingErrors = recordsWithLinting
+	summary.AllLintingMessageCount = allLinting
+	summary.FormatsWithBadHeuristics = badHeuristics
 	for _, wd := range wikidataMapping {
-		if len(wd.Signatures) > 1 {
-			summary.MultipleSequences++
-			summary.Multiples = append(summary.Multiples, wd.URI)
+		if len(wd.Signatures) > 0 {
+			summary.RecordsWithSignatures++
 		}
-		for _, signature := range wd.Signatures {
-			signature.analyseSignature(summary, wd.URI)
-		}
-		if len(wd.Signatures) != 0 {
-			summary.FormatsWithSignatures++
+		for _, sigs := range wd.Signatures {
+			if len(sigs.ByteSequences) > 1 {
+				summary.MultipleSequences++
+			}
 		}
 	}
 }
 
+// runSPARQL ...
 func runSPARQL() []map[string]spargo.Item {
 	sparqlMe := spargo.SPARQLClient{}
-	sparqlMe.ClientInit(url, query)
+	sparqlMe.ClientInit(url, strings.Replace(query, langTemplate, lang, 1))
 	res := sparqlMe.SPARQLGo()
+	f, _ := os.Create("res.json")
+	defer f.Close()
+	f.Write([]byte(res.Human))
 	return res.Results.Bindings
 }
 
@@ -189,37 +204,33 @@ func main() {
 	flag.Parse()
 	results := runSPARQL()
 	var summary Summary
+
+	var expectedRecordsWithSignatures = make(map[string]bool)
+	var allRecordsInclusive = make(map[string]bool)
+
 	for _, wdRecord := range results {
-		id := getID(wdRecord[formatField].Value)
+		allRecordsInclusive[wdRecord[uriField].Value] = true
+		id := getID(wdRecord[uriField].Value)
+		if wdRecord[signatureField].Value != "" {
+			summary.SparqlRowsWithSigs++
+			expectedRecordsWithSignatures[wdRecord[uriField].Value] = true
+		}
 		if wikidataMapping[id].ID == "" {
-			wikidataMapping[id] = newRecord(wdRecord)
+			add := addSignatures(results, id)
+			wikidataMapping[id] = newRecord(wdRecord, add)
 		} else {
 			wikidataMapping[id] = updateRecord(wdRecord, wikidataMapping[id])
 		}
 	}
+
 	summary.AllSparqlResults = len(results)
 	summary.CondensedSparqlResults = len(wikidataMapping)
+	summary.RecordsWithPotentialSignatures = len(expectedRecordsWithSignatures)
 	analyseWikidataRecords(&summary)
+
+	// WIKIDATA TODO: Flag to show linting errors specific to Wikidata.
 	if debug {
-		out := ""
-		for _, wd := range wikidataMapping {
-			if len(wd.Signatures) > threshold {
-				for _, signature := range wd.Signatures {
-					if !csv {
-						out = fmt.Sprintf("%s%s,", out, signature)
-					} else {
-						out = fmt.Sprintf("%s%s\n", out, signature.CSV(wd.URI, len(wd.Signatures)))
-					}
-				}
-			}
-		}
-		if !csv {
-			fmt.Fprintf(os.Stdout, "[%s]", strings.Trim(out, ","))
-			return
-		}
-		const header = "uri, count, sig, provenance, date, encoding, relativity"
-		fmt.Fprintf(os.Stdout, "%s\n%s", header, out)
-	} else {
-		fmt.Fprintf(os.Stdout, "%s\n", summary)
+		summary.AllLintingMessages = lintingToString()
 	}
+	fmt.Fprintf(os.Stdout, "%s\n", summary)
 }
